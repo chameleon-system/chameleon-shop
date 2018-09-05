e1c6025150e39d27a36d2fb93fd0a5ba5e50727e
Ulrich Kuhn <ulrich.kuhn@esono.de>
2018-09-05 13:08:10 +0200

    ref #50: Change doc of field name

--- src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1535963426.inc.php
+++ src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1535963426.inc.php
@@ -8,4 +8,4 @@
 TCMSLogChange::addInfoMessage("Check table 'shop_article' if it includes the manufacturer name in the list display (and thus the list query). 
     If not it might be advisable to add it in order to make the name searchable in the backend table.
     1. Change list query: add '`shop_manufacturer`.`name` as manufacturer' and the respective JOIN
-    2. Change the existing display field 'Manufacturer id' to show the name instead", TCMSLogChange::INFO_MESSAGE_LEVEL_TODO);
+    2. Change the existing display field 'Manufacturer ID' to show the name instead", TCMSLogChange::INFO_MESSAGE_LEVEL_TODO);
