0d2a37fc2c1b513f1252a397f24fd67c0d747b20
Ulrich Kuhn <ulrich.kuhn@esono.de>
2018-12-19 10:30:35 +0100

    ref #243: deprecate unused classes

--- src/ShopCurrencyBundle/pkgShop/objects/db/TPkgShopCurrency_ShopDiscount.class.php
+++ src/ShopCurrencyBundle/pkgShop/objects/db/TPkgShopCurrency_ShopDiscount.class.php
@@ -9,6 +9,9 @@
  * file that was distributed with this source code.
  */
 
+/**
+ * @deprecated since 6.3.0 - no longer used
+ */
 class TPkgShopCurrency_ShopDiscount extends TPkgShopCurrency_ShopDiscountAutoParent
 {
 }
