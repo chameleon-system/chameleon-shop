6d7d87625f2388dcb0b41a2d28a9be996b78536e
Philipp Speck <philipp.speck@esono.de>
2019-01-15 09:55:20 +0100

    ref #234: add update to add currency voucher fields (refactor)

--- src/ShopCurrencyBundle/Bridge/Chameleon/Migration/Script/update-1545232400.inc.php
+++ src/ShopCurrencyBundle/Bridge/Chameleon/Migration/Script/update-1545232400.inc.php
@@ -9,39 +9,6 @@ $fieldId = TCMSLogChange::createUnusedRecordId('cms_field_conf');
 
 $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
   ->setFields([
-      'cms_tbl_conf_id' => TCMSLogChange::GetTableId('shop_voucher_use'),
-      'name' => 'new_field',
-      'translation' => '',
-      'cms_field_type_id' => TCMSLogChange::GetFieldType('CMSFIELD_STRING'),
-      'cms_tbl_field_tab' => '',
-      'isrequired' => '0',
-      'fieldclass' => '',
-      'fieldclass_subtype' => '',
-      'class_type' => 'Core',
-      'modifier' => 'none',
-      'field_default_value' => '',
-      'length_set' => '',
-      'fieldtype_config' => '',
-      'restrict_to_groups' => '0',
-      'field_width' => '',
-      'position' => '0',
-      '049_helptext' => '',
-      'row_hexcolor' => '',
-      'is_translatable' => '0',
-      'validation_regex' => '',
-      'id' => $fieldId,
-
-  ])
-;
-TCMSLogChange::insert(__LINE__, $data);
-
-$query ="ALTER TABLE `shop_voucher_use`
-                        ADD `value_used_in_order_currency` DECIMAL(10,2) NOT NULL COMMENT 'Value consumed in the order currency: Displays the consumption value of the voucher in the currency in which the order was made at the time of ordering.'";
-TCMSLogChange::RunQuery(__LINE__, $query);
-
-
-$data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
-  ->setFields([
       'name' => 'value_used_in_order_currency',
       'translation' => 'Value consumed in the order currency',
       'cms_field_type_id' => TCMSLogChange::GetFieldType('CMSFIELD_DECIMAL'),
@@ -59,17 +26,22 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
       'row_hexcolor' => '',
       'is_translatable' => '0',
       'validation_regex' => '',
-  ])
-  ->setWhereEquals([
+      'cms_tbl_conf_id' => TCMSLogChange::GetTableId('shop_voucher_use'),
+      'fieldclass_subtype' => '',
+      'class_type' => 'Core',
       'id' => $fieldId,
   ])
 ;
-TCMSLogChange::update(__LINE__, $data);
+TCMSLogChange::insert(__LINE__, $data);
+
+$query = "ALTER TABLE `shop_voucher_use`
+                        ADD `value_used_in_order_currency` DECIMAL(10,2) NOT NULL COMMENT 'Value consumed in the order currency: Displays the consumption value of the voucher in the currency in which the order was made at the time of ordering.'";
+TCMSLogChange::RunQuery(__LINE__, $query);
 
 $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
     ->setFields([
         'translation' => 'Wert verbraucht in Währung der Bestellung',
-        '049_helptext' => 'Zeigt den Verbrauchswert des Gutscheins in der Währung an, in der die Bestellung getätigt wurde zum Zeitpunkt der Bestellung.',
+        '049_helptext' => 'Zeigt den Verbrauchswert des Gutscheins (zum Zeitpunkt der Bestellung) in der Währung an, in der die Bestellung getätigt wurde.',
     ])
     ->setWhereEquals([
         'id' => $fieldId,
@@ -77,42 +49,7 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
 ;
 TCMSLogChange::update(__LINE__, $data);
 
-
-
-
 $fieldId = TCMSLogChange::createUnusedRecordId('cms_field_conf');
-
-$data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
-  ->setFields([
-      'cms_tbl_conf_id' => TCMSLogChange::GetTableId('shop_voucher_use'),
-      'name' => 'new_field',
-      'translation' => '',
-      'cms_field_type_id' => TCMSLogChange::GetFieldType('CMSFIELD_STRING'),
-      'cms_tbl_field_tab' => '',
-      'isrequired' => '0',
-      'fieldclass' => '',
-      'fieldclass_subtype' => '',
-      'class_type' => 'Core',
-      'modifier' => 'none',
-      'field_default_value' => '',
-      'length_set' => '',
-      'fieldtype_config' => '',
-      'restrict_to_groups' => '0',
-      'field_width' => '',
-      'position' => '0',
-      '049_helptext' => '',
-      'row_hexcolor' => '',
-      'is_translatable' => '0',
-      'validation_regex' => '',
-      'id' => $fieldId,
-  ])
-;
-TCMSLogChange::insert(__LINE__, $data);
-
-$query ="ALTER TABLE `shop_voucher_use`
-                        ADD `pkg_shop_currency_id` CHAR(36) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL COMMENT 'Currency in which the order was made: '";
-TCMSLogChange::RunQuery(__LINE__, $query);
-
 
 $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
   ->setFields([
@@ -133,16 +70,21 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
       'row_hexcolor' => '',
       'is_translatable' => '0',
       'validation_regex' => '',
-  ])
-  ->setWhereEquals([
+      'cms_tbl_conf_id' => TCMSLogChange::GetTableId('shop_voucher_use'),
+      'fieldclass_subtype' => '',
+      'class_type' => 'Core',
       'id' => $fieldId,
   ])
 ;
-TCMSLogChange::update(__LINE__, $data);
+TCMSLogChange::insert(__LINE__, $data);
+
+$query = "ALTER TABLE `shop_voucher_use`
+                        ADD `pkg_shop_currency_id` CHAR(36) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL COMMENT 'Currency in which the order was made: '";
+TCMSLogChange::RunQuery(__LINE__, $query);
 
 $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
     ->setFields([
-        'translation' => 'Währung in der die Bestellung getätigt wurde.',
+        'translation' => 'Währung, in der die Bestellung getätigt wurde',
     ])
     ->setWhereEquals([
         'id' => $fieldId,
@@ -150,6 +92,5 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
 ;
 TCMSLogChange::update(__LINE__, $data);
 
-$query ="ALTER TABLE `shop_voucher_use` ADD INDEX ( `pkg_shop_currency_id` )";
+$query = 'ALTER TABLE `shop_voucher_use` ADD INDEX ( `pkg_shop_currency_id` )';
 TCMSLogChange::RunQuery(__LINE__, $query);
-
