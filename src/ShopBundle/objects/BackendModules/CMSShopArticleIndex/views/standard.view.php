d204a607deb1a57d2617d54773beb2dd8efe5d0a
Carmen Kleis <carmen.kleis@esono.de>
2019-02-14 14:47:25 +0100

    ref #299: add TGlobal::OutHTML() to translations

--- src/ShopBundle/objects/BackendModules/CMSShopArticleIndex/views/standard.view.php
+++ src/ShopBundle/objects/BackendModules/CMSShopArticleIndex/views/standard.view.php
@@ -10,7 +10,7 @@ $translator = ServiceLocator::get('translator');
 
 <div class="card">
     <div class="card-header">
-        <?= $translator->trans('chameleon_system_shop.product_search_index.headline'); ?>
+        <?= TGlobal::OutHTML($translator->trans('chameleon_system_shop.product_search_index.headline')); ?>
     </div>
     <div class="card-body">
         <form name="indexer" method="post" action="<?=PATH_CMS_CONTROLLER; ?>" accept-charset="UTF-8">
@@ -21,14 +21,14 @@ $translator = ServiceLocator::get('translator');
 
             <?php if (!$data['bIndexIsRunning'] || $data['bIndexCompleted']) {
                 ?>
-                <input class="btn btn-primary" type="submit" value="<?= $translator->trans('chameleon_system_shop.product_search_index.generate'); ?>"/>
+                <input class="btn btn-primary" type="submit" value="<?= TGlobal::OutHTML($translator->trans('chameleon_system_shop.product_search_index.generate')); ?>"/>
                 <?php
             } ?>
         </form>
         <?php
         $oLocal = &TCMSLocal::GetActive();
         ?>
-        <div class="mt-4"><?= $translator->trans('chameleon_system_shop.product_search_index.completed'); ?>: <?=$oLocal->FormatNumber($data['bPercentDone'], 2); ?>%</div>
+        <div class="mt-4"><?= TGlobal::OutHTML($translator->trans('chameleon_system_shop.product_search_index.completed')); ?>: <?=$oLocal->FormatNumber($data['bPercentDone'], 2); ?>%</div>
 
         <?php if (!$data['bIndexCompleted'] && $data['bIndexIsRunning']) {
             ?>
