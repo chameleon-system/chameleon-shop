2f311562c2c3e1a5f76c8159b26fd7450b789aa4
Johannes Hertenstein <git@j6s.dev>
2021-03-23 10:44:24 +0100

    ref #636: Use query result directly

--- src/EcommerceStatsBundle/Service/TopSellerService.php
+++ src/EcommerceStatsBundle/Service/TopSellerService.php
@@ -14,6 +14,7 @@ namespace ChameleonSystem\EcommerceStatsBundle\Service;
 use ChameleonSystem\EcommerceStatsBundle\DataModel\ShopOrderItemDataModel;
 use ChameleonSystem\EcommerceStatsBundle\Interfaces\TopSellerServiceInterface;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
 
 class TopSellerService implements TopSellerServiceInterface
 {
@@ -37,12 +38,14 @@ class TopSellerService implements TopSellerServiceInterface
         string $portalId,
         int $limit = 50
     ): array {
-        $query = '
+
+        $query = sprintf('
             SELECT 
                 SUM(`shop_order_item`.`order_amount`) AS totalordered,
                 SUM(`shop_order_item`.`order_price_after_discounts`) AS totalorderedvalue,
                 `shop_category`.`url_path` AS categorypath, 
-                `shop_order_item`.*
+                `shop_order_item`.`articlenumber`,
+                `shop_order_item`.`name`
             FROM `shop_order_item`
                 LEFT JOIN `shop_order`
                     ON `shop_order_item`.`shop_order_id` = `shop_order`.`id`
@@ -50,44 +53,60 @@ class TopSellerService implements TopSellerServiceInterface
                     ON `shop_order_item`.`shop_article_id` = `shop_article_shop_category_mlt`.`source_id`
                 LEFT JOIN `shop_category` 
                     ON `shop_article_shop_category_mlt`.`target_id` = `shop_category`.`id`
-               ';
-        $baseConditionList = [];
-        if (null !== $startDate) {
-            $baseConditionList[] = '`shop_order`.`datecreated` >= '.$this->connection->quote($startDate->format('Y-m-d H:i:s'));
-        }
-        if (null !== $endDate) {
-            $baseConditionList[] = '`shop_order`.`datecreated` <= '.$this->connection->quote($endDate->format('Y-m-d H:i:s'));
-        }
-        if ('' !== $portalId) {
-            $baseConditionList[] = '`shop_order`.`cms_portal_id` = '.$this->connection->quote($portalId);
-        }
-
-        if (count($baseConditionList) > 0) {
-            $query .= ' WHERE ('.implode(') AND (', $baseConditionList).')';
-        }
-
-        $query .= ' GROUP BY `shop_category`.`id`, `shop_order_item`.`shop_article_id`';
-        $query .= ' ORDER BY totalordered DESC ';
-        $query .= ' LIMIT 0,'.$limit;
+            %1$s
+            GROUP BY `shop_category`.`id`, `shop_order_item`.`shop_article_id`
+            ORDER BY `totalordered` DESC
+            LIMIT 0,%2$d
+               ',
+            $this->getWhereQueryPart($startDate, $endDate, $portalId),
+            $limit
+        );
 
         $items = [];
-        $list = \TdbShopOrderItemList::GetList($query);
-        while ($item = $list->Next()) {
-            $items[] = $this->tdbToDataModel($item);
+        foreach ($this->executeQuery($query) as $row) {
+            $items[] = new ShopOrderItemDataModel(
+                (string) $row['articlenumber'],
+                (string) $row['name'],
+                (int) $row['totalordered'],
+                (float) $row['totalorderedvalue'],
+                (string) $row['categorypath']
+            );
         }
 
         return $items;
     }
 
-    private function tdbToDataModel(\TdbShopOrderItem $tdb): ShopOrderItemDataModel
+    /**
+     * Returns the `WHERE` query part including the `WHERE` keyword if
+     * conditions apply or and empty string if none apply.
+     *
+     * The returned string is safe to use in a query.
+     */
+    private function getWhereQueryPart(?\DateTime $startDate, ?\DateTime $endDate, string $portalId): string
     {
-        return new ShopOrderItemDataModel(
-            $tdb->fieldArticlenumber,
-            $tdb->fieldName,
-            (int) $tdb->sqlData['totalordered'],
-            (float) $tdb->sqlData['totalorderedvalue'],
-            $tdb->sqlData['categorypath']
-        );
+        $conditions = [];
+        if (null !== $startDate) {
+            $conditions[] = '`shop_order`.`datecreated` >= '.$this->connection->quote($startDate->format('Y-m-d H:i:s'));
+        }
+        if (null !== $endDate) {
+            $conditions[] = '`shop_order`.`datecreated` <= '.$this->connection->quote($endDate->format('Y-m-d H:i:s'));
+        }
+        if ('' !== $portalId) {
+            $conditions[] = '`shop_order`.`cms_portal_id` = '.$this->connection->quote($portalId);
+        }
+
+        if (0 === count($conditions)) {
+            return '';
+        }
+
+        return 'WHERE ('.implode(') AND (', $conditions).')';
     }
 
+    private function executeQuery(string $query): \Generator
+    {
+        $results = $this->connection->executeQuery($query);
+        while ($row = $results->fetch(FetchMode::ASSOCIATIVE)) {
+            yield $row;
+        }
+    }
 }
