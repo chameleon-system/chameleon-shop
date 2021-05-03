195ae83a95057d70317e02daae769f970aa7994d
FinnBrackrock <finn.brackrock@gmail.com>
2021-05-03 12:08:12 +0200

    fix wrong filter numeric slider test

--- src/ShopListFilterBundle/Tests/mappers/filter/TPkgShopListfilterMapper_FilterNumericSliderTest.php
+++ src/ShopListFilterBundle/Tests/mappers/filter/TPkgShopListfilterMapper_FilterNumericSliderTest.php
@@ -1,53 +1,78 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace ChameleonSystem\ShopListFilterBundle\Tests\mappers\filter;
 
 use ChameleonSystem\ShopListFilterBundle\Tests\mappers\filter\Mocks\VisitorMock;
 use IMapperCacheTriggerRestricted;
-use TPkgShopListfilterMapper_FilterNumericSlider;
 use PHPUnit\Framework\TestCase;
+use TPkgShopListfilterMapper_FilterNumericSlider;
 
 class TPkgShopListfilterMapper_FilterNumericSliderTest extends TestCase
 {
     public function provideMinAndMax(): array
     {
         return [
-            [ 30, 94, 100 ],
-            [ 20, 50, 60 ],
-            [ 0, 10, 15 ],
+            [30, 94],
+            [20, 50],
+            [0, 10],
         ];
     }
 
     /** @dataProvider provideMinAndMax */
     public function testSetsMaxValueOfSliderToAtLeastMaxValueOfData(
         int $min,
-        int $max,
-        int $expectedMaxBound
+        int $max
     ): void {
         $mapper = new TPkgShopListfilterMapper_FilterNumericSlider();
 
         $visitor = new VisitorMock([
-            'oFilterItem' => new class ($min, $max) {
+            'oFilterItem' => new class($min, $max) {
                 public $fieldName = 'foo';
                 private $options;
 
                 public function __construct(int $min, int $max)
                 {
-                    $this->options = [ $min => 1, $max => 1];
+                    $this->options = [$min => 1, $max => 1];
                 }
 
-                public function GetURLInputName() { return ''; }
-                public function GetAddFilterURL() { return ''; }
-                public function IsActive() { return true; }
-                public function GetActiveStartValue() { return 0; }
-                public function GetActiveEndValue() { return 0; }
-                public function GetOptions() { return $this->options; }
-            },
-            'oActiveFilter' => new class {
-                public function GetCurrentFilterAsArray() {
-                    return [ ];
+                public function GetURLInputName()
+                {
+                    return '';
                 }
-            }
+
+                public function GetAddFilterURL()
+                {
+                    return '';
+                }
+
+                public function IsActive()
+                {
+                    return true;
+                }
+
+                public function GetActiveStartValue()
+                {
+                    return 0;
+                }
+
+                public function GetActiveEndValue()
+                {
+                    return 0;
+                }
+
+                public function GetOptions()
+                {
+                    return $this->options;
+                }
+            },
+            'oActiveFilter' => new class() {
+                public function GetCurrentFilterAsArray()
+                {
+                    return [];
+                }
+            },
         ]);
 
         $oCacheTriggerManager = $this->createMock(IMapperCacheTriggerRestricted::class);
@@ -57,6 +82,5 @@ class TPkgShopListfilterMapper_FilterNumericSliderTest extends TestCase
         /** @var \Slider $slider */
         $slider = $visitor->mappedValues['slider'];
         $this->assertGreaterThanOrEqual($max, $slider->getMax());
-        $this->assertLessThanOrEqual($expectedMaxBound, $slider->getMax());
     }
 }
