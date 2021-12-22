4fadefcf56126a4d8a06a6c8d51c7cfcd03754c5
Ulrich Kuhn <ulrich.kuhn@esono.de>
2021-12-22 13:12:20 +0100

    ref #584: Execute message update for all messages (in multiple portals)

--- src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1629872884.inc.php
+++ src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1629872884.inc.php
@@ -7,30 +7,30 @@
 
 $connection = TCMSLogChange::getDatabaseConnection();
 
-$id = $connection->fetchOne("SELECT `id` FROM `cms_message_manager_message` WHERE `message` =
+$ids = $connection->fetchFirstColumn("SELECT `id` FROM `cms_message_manager_message` WHERE `message` =
 'Für die Verwendung dieses Gutscheins ist ein Mindestbestellwert von [{TdbShopVoucherSeries__fieldRestrictToValueFormated}] erforderlich. Bitte beachten Sie, dass buchpreisgebundene Artikel nur bei gesponserten Gutscheinen zum Warenwert gezählt werden.'
 OR `message` =
 'A minimum order value of [{TdbShopVoucherSeries__fieldRestrictToValueFormated}] is required to use this voucher. Please note that articles with a fixed retail prices such as books are added to the value of goods only when using a sponsored voucher.'
 ");
 
-if (false !== $id) {
-    $data = TCMSLogChange::createMigrationQueryData('cms_message_manager_message', 'de')
-        ->setFields([
-            'message' => 'Für die Verwendung dieses Gutscheins ist ein Mindestbestellwert von [{TdbShopVoucherSeries__fieldRestrictToValueFormated}] erforderlich. Bitte beachten Sie, dass buchpreisgebundene Artikel nur bei gekauften Gutscheinen zum Warenwert gezählt werden. Auf Aktionsgutscheine können buchpreisgebundene Artikel leider nicht berücksichtigt werden.',
-        ])
-        ->setWhereEquals([
-            'id' => $id,
-        ])
-    ;
-    TCMSLogChange::update(__LINE__, $data);
+if (\count($ids) > 0) {
+    foreach ($ids as $id) {
+        $data = TCMSLogChange::createMigrationQueryData('cms_message_manager_message', 'de')
+            ->setFields([
+                'message' => 'Für die Verwendung dieses Gutscheins ist ein Mindestbestellwert von [{TdbShopVoucherSeries__fieldRestrictToValueFormated}] erforderlich. Bitte beachten Sie, dass buchpreisgebundene Artikel nur bei gekauften Gutscheinen zum Warenwert gezählt werden. Für Aktionsgutscheine können buchpreisgebundene Artikel leider nicht berücksichtigt werden.',
+            ])
+            ->setWhereEquals([
+                'id' => $id,
+            ]);
+        TCMSLogChange::update(__LINE__, $data);
 
-    $data = TCMSLogChange::createMigrationQueryData('cms_message_manager_message', 'en')
-        ->setFields([
-            'message' => 'For the use of this voucher a minimum order value of [{TdbShopVoucherSeries__fieldRestrictToValueFormated}] is required. Please note that book-price-bound articles are only counted towards the value of goods in the case of purchased vouchers. Unfortunately, book-price related articles cannot be included in promotion vouchers.',
-        ])
-        ->setWhereEquals([
-            'id' => $id,
-        ])
-    ;
-    TCMSLogChange::update(__LINE__, $data);
+        $data = TCMSLogChange::createMigrationQueryData('cms_message_manager_message', 'en')
+            ->setFields([
+                'message' => 'For the use of this voucher a minimum order value of [{TdbShopVoucherSeries__fieldRestrictToValueFormated}] is required. Please note that book-price-bound articles are only counted towards the value of goods in the case of purchased vouchers. Unfortunately, book-price related articles cannot be included in promotion vouchers.',
+            ])
+            ->setWhereEquals([
+                'id' => $id,
+            ]);
+        TCMSLogChange::update(__LINE__, $data);
+    }
 }
