c33933bc3479ff0095f85adbd549358026e1c173
Robert Nürnberg <robert.nuernberg@esono.de>
2024-04-25 18:09:45 +0200

    ref #62986: fix typo

--- src/ShopArticlePreorderBundle/EventListener/UpdateProductStockListener.php
+++ src/ShopArticlePreorderBundle/EventListener/UpdateProductStockListener.php
@@ -6,7 +6,7 @@ use ChameleonSystem\ShopBundle\Event\UpdateProductStockEvent;
 
 class UpdateProductStockListener
 {
-    public function sentPreOrderMail(UpdateProductStockEvent $event): void
+    public function sendPreOrderMail(UpdateProductStockEvent $event): void
     {
         if ($event->getOldStock() < 1 && $event->getNewStock() > 0) {
             $oShopArticlePreorderList = \TdbPkgShopArticlePreorderList::GetListForShopArticleId($event->getProductId());
