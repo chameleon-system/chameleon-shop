8fe950ca3898c191fb885f5dcb69b3fa28796d93
Sven Jansen <sven.jansen@esono.de>
2024-12-19 16:01:44 +0100

    ref #65248: add AmazonPaymentExtranetUser stub

---
+++ src/AmazonPaymentBundle/pkgExtrnateUser/AmazonPaymentExtranetUser.php
@@ -0,0 +1,10 @@
+<?php
+namespace ChameleonSystem\AmazonPaymentBundle\pkgExtranetUser;
+
+/**
+ * @deprecated in 8.0.0 - reference will be removed by migration
+ */
+class AmazonPaymentExtranetUser extends \ChameleonSystemAmazonPaymentBundlepkgExtranetUserAmazonPaymentExtranetUserAutoParent
+{
+
+}
