17d013a50e33ce6e7c049d96ea682a3fe67c5ffa
Markus Haendler <markus.haendler@esono.de>
2025-03-17 10:43:20 +0100

    ref #66055: removed the tableditor as we cannot use it in a updatescript

--- src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1742202279.inc.php
+++ src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1742202279.inc.php
@@ -1,4 +1,4 @@
-<h1>Build <div id="17"></div></h1>
+<h1>Build #1742202279</h1>
 <h2>Date: 2025-03-17</h2>
 <div class="changelog">
     - ref #66055: removed outdated smart url handler
@@ -16,13 +16,14 @@ foreach ($outDatedSmartUrlHandler as $smartUrlHandler) {
         'SELECT id FROM `cms_smart_url_handler` WHERE name = :name',
         ['name' => $smartUrlHandler]
     );
-    if(false === $smartUrlHandlerId) {
+    if (false === $smartUrlHandlerId) {
         continue;
     }
 
-    $tableEditor = \TTools::GetTableEditorManager('cms_smart_url_handler');
-    $tableEditor->AllowDeleteByAll(true);
-
-    //Delete everything references
-    $tableEditor->Delete($smartUrlHandlerId);
+    $connection->delete('cms_smart_url_handler', [
+        'id' => $smartUrlHandlerId
+    ]);
+    $connection->delete('cms_smart_url_handler_cms_portal_mlt', [
+        'source_id' => $smartUrlHandlerId
+    ]);
 }
