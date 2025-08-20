6eadeb4b8f49ac64b5beaefe1b5f448d1b72d453
Nico Klein <nico.klein@esono.de>
2025-08-20 11:38:31 +0200

    ref #67464: add is array to clarify if statement

--- src/ShopBundle/objects/db/TShopOrderStep/TShopStepUserDataV2EndPoint.class.php
+++ src/ShopBundle/objects/db/TShopOrderStep/TShopStepUserDataV2EndPoint.class.php
@@ -779,7 +779,7 @@ class TShopStepUserDataV2EndPoint extends TdbShopOrderStep
             }
             $aNotAllowed = ['id', 'name', 'cmsident', 'password', 'session_key', 'login_timestamp', 'login_salt', 'shop_id', 'datecreated', 'tmpconfirmkey', 'confirmed', 'confirmedon', 'reg_email_send'];
 
-            if ($aPrimaryAddress) {
+            if (is_array($aPrimaryAddress)) {
                 foreach ($aPrimaryAddress as $sField => $sValue) {
                     if (!in_array($sField, $aNotAllowed)) {
                         $aUserData[$sField] = $sValue;
