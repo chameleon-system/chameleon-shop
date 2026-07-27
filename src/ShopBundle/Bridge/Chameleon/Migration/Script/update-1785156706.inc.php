7269e0a9b31c7cad5ea0db22966ca4d2da8288e1
Markus Haendler <markus.haendler@esono.de>
2026-07-27 16:30:00 +0200

    ref #70900: make pkg_shop_listfilter_item and shop_order_step translatable

---
+++ src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1785156706.inc.php
@@ -0,0 +1,8 @@
+<h1>Build #1785156706</h1>
+<h2>Date: 2026-07-27</h2>
+<div class="changelog">
+    - ref #70900: make headline of shop_order_step translatable
+</div>
+<?php
+
+TCMSLogChange::makeFieldMultilingual('shop_order_step', 'headline');
---
+++ src/ShopListFilterBundle/Bridge/Chameleon/Migration/Script/update-1785153989.inc.php
@@ -0,0 +1,8 @@
+<h1>Build #1785153989</h1>
+<h2>Date: 2026-07-27</h2>
+<div class="changelog">
+    - ref #70900: make shop listfilter translatable
+</div>
+<?php
+
+TCMSLogChange::makeFieldMultilingual('pkg_shop_listfilter_item', 'name');
