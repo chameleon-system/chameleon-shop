3e34a4d6e590cdc3d247d1c639b7e45952c4b50f
Carmen Kleis <carmen.kleis@esono.de>
2026-08-13 09:58:55 +0200

    ref #71065: fix update for missing field customer_service_telephone_info

--- src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1785313357.inc.php
+++ src/ShopBundle/Bridge/Chameleon/Migration/Script/update-1785313357.inc.php
@@ -5,4 +5,9 @@
 </div>
 <?php
 
-TCMSLogChange::makeFieldMultilingual('shop', 'customer_service_telephone_info');
+//this isn't a field from chameleon_shop and I didn't find it in any core code.
+//This update actually belongs in a client project
+
+if (TCMSLogChange::FieldExists('shop', 'customer_service_telephone_info')) {
+    TCMSLogChange::makeFieldMultilingual('shop', 'customer_service_telephone_info');
+}
